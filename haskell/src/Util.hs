76ce06924b4e4b3a79bd874867adaf140173c5c3
MANGO1234 <814583+MANGO1234@users.noreply.github.com>
2016-01-08 15:39:28 -0800

    Update

--- haskell/src/Util.hs
+++ haskell/src/Util.hs
@@ -19,7 +19,28 @@ foldt f xs = foldt f (fmap (foldl1 f) (pairs xs))
 merge :: (Ord a) => [a] -> [a] -> [a]
 merge xx [] = xx
 merge [] yy = yy
-merge xx@(x:xs) yy@(y:ys) = if x < y then x:merge xs yy else y:merge xx ys
+merge xx@(x:xs) yy@(y:ys) = if x <= y then x:merge xs yy else y:merge xx ys
 
 kmerge :: (Ord a) => [[a]] -> [a]
 kmerge = foldt merge
+
+-- merge list of sorted list where the lists in the infinite list are sorted by first element, allows infinite lists
+kmergei :: (Ord a) => [[a]] -> [a]
+kmergei = kmergeiBySort compare
+
+kmergeiBySort :: (a -> a -> Ordering) -> [[a]] -> [a]
+kmergeiBySort comp = kmergeii
+    where kmergeii [] = []
+          kmergeii [x] = x
+          kmergeii ([]:ls) = kmergeii ls
+          kmergeii ((x:xs):ls) = x:kmergeii (insert xs ls)
+          insert [] ls = ls
+          insert as [] = [as]
+          insert as ([]:ls) = insert as ls
+          insert as@(a:_) ll@(xx@(x:xs):ls) = case comp a x of
+              LT -> as:ll
+              EQ -> as:ll
+              GT -> xx:insert as ls
+
+cross :: (a -> b -> c) -> [a] -> [b] -> [[c]]
+cross f as bs = fmap (\a -> fmap (f a) bs) as
