73abcdb0f17fa0faf1b9e9a83118dc7ff9b2b3e6
De Li <de_li@live.com>
2017-07-22 11:52:18 -0700

    P166

--- python/main.py
+++ python/main.py
@@ -5,74 +5,49 @@ from euler_util import *
 
 START = time.time()
 
-# basic idea: calculate each probability of ant being at a square after 50 rounds, then for each square
-# calculate expected value of no ant being there and sum up all the squares
-# no taking into account symmetry but it's super fast to code
+t = [[] for i in range(0, 37)]
 
-# (899/900)^900*900 will offer an answer to the nearest integer (330)
-# probably over the long term, each ant will have 1/900 of being at a square
-
-x = []
-
-for i in range(0, 30):
-    a = []
-    for j in range(0, 30):
-        k = np.zeros((30, 30))
-        k[i][j] = 1
-        a.append(k)
-    x.append(a)
-
-t = [x]
-
-for i in range(1, 51):
-    a = []
-    for j in range(0, 30):
-        b = []
-        for k in range(0, 30):
-            b.append(None)
-        a.append(b)
-    t.append(a)
+for i in range(0, 10):
+    for j in range(0, 10):
+        for k in range(0, 10):
+            for l in range(0, 10):
+                t[i + j + k + l].append([i, j, k, l])
 
 
-def f(n, i, j):
-    if t[n][i][j] is not None:
-        return t[n][i][j]
-    if i == 0:
-        if j == 0:
-            t[n][i][j] = f(n - 1, i + 1, j) / 2 + f(n - 1, i, j + 1) / 2
-        elif j == 29:
-            t[n][i][j] = f(n - 1, i + 1, j) / 2 + f(n - 1, i, j - 1) / 2
-        else:
-            t[n][i][j] = f(n - 1, i + 1, j) / 3 + f(n - 1, i, j - 1) / 3 + f(n - 1, i, j + 1) / 3
-    elif i == 29:
-        if j == 0:
-            t[n][i][j] = f(n - 1, i - 1, j) / 2 + f(n - 1, i, j + 1) / 2
-        elif j == 29:
-            t[n][i][j] = f(n - 1, i - 1, j) / 2 + f(n - 1, i, j - 1) / 2
-        else:
-            t[n][i][j] = f(n - 1, i - 1, j) / 3 + f(n - 1, i, j - 1) / 3 + f(n - 1, i, j + 1) / 3
-    elif j == 0:
-        t[n][i][j] = f(n - 1, i, j + 1) / 3 + f(n - 1, i - 1, j) / 3 + f(n - 1, i + 1, j) / 3
-    elif j == 29:
-        t[n][i][j] = f(n - 1, i, j - 1) / 3 + f(n - 1, i - 1, j) / 3 + f(n - 1, i + 1, j) / 3
-    else:
-        t[n][i][j] = f(n - 1, i, j - 1) / 4 + f(n - 1, i, j + 1) / 4 + f(n - 1, i - 1, j) / 4 + f(n - 1, i + 1, j) / 4
-    return t[n][i][j]
+def f(n, xs):
+    tn = 0
+    for x in xs:
+        for y in xs:
+            for b31 in range(min(max(n - x[0] - y[0] - 9, 0), 9), max(min(n - x[0] - y[0] + 1, 10), 0)):
+                b41 = n - x[0] - y[0] - b31
+                b32 = n - x[3] - y[2] - b41
+                if b32 < 0 or b32 > 9:
+                    continue
+                b42 = n - x[1] - y[1] - b32
+                if b42 < 0 or b42 > 9:
+                    continue
+                for b34 in range(min(max(n - x[3] - y[3] - 9, 0), 9), max(min(n - x[3] - y[3] + 1, 10), 0)):
+                    b44 = n - x[3] - y[3] - b34
+                    b33 = n - x[0] - y[1] - b44
+                    if b33 < 0 or b33 > 9:
+                        continue
+                    if b31 + b32 + b33 + b34 != n:
+                        continue
+                    b43 = n - x[2] - y[2] - b33
+                    if b43 < 0 or b43 > 9:
+                        continue
+                    if b41 + b42 + b43 + b44 != n:
+                        continue
+                    tn += 1
+    return tn
 
-for i in range(0, 30):
-    for j in range(0, 30):
-        f(50, i, j)
 
-t = t[50]
-n = 0
-for i1 in range(0, 30):
-    for j1 in range(0, 30):
-        tmp = 1
-        for i2 in range(0, 30):
-            for j2 in range(0, 30):
-                tmp *= (1 - t[i2][j2][i1][j1])
-        n += tmp
-print(n)
+total = 0
+for i in range(0, 18):
+    total += f(i, t[i]) * 2
+    print(i)
+total += f(18, t[18])
+print(total)
 
 END = time.time()
 print(END - START)
